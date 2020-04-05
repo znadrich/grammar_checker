d54649aa6b8fec20b681b7b06d76b40f3efa1ea1
znadrich <znadrich@gmail.com>
2020-04-05 16:40:56 -0400

    tokenize random text

--- random_text.py
+++ random_text.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from nltk.tokenize import sent_tokenize
 
 def random_text():
     t = requests.get('https://randomtextgenerator.com/').text
     soup = BeautifulSoup(t, 'html.parser')
-    return soup.body.find('textarea').contents[0]
+    paragraph = soup.body.find('textarea').contents[0]
+    sentences = sent_tokenize(paragraph)
+    return sentences
